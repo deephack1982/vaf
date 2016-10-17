2313d409ceb236aa4ababdc7ced33835cb4da423
Mark Dickie <markd@mavennetwork.co.uk>
2016-10-17 22:55:18 +0100

    Added seeds for airfields, squadrons and Me!  Also expanded the aircraft types in squadron model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Airfield.create(name: 'Kutaisi', airfield_type: 'Airfield', ATC_channel: '134.200', ATIS_channel: '135.000')
+  Squadron.create(name: '870(R)', aircraft: 'L39C', airfield_id: '1')
+  Pilot.create(name: 'Mark Dickie', email: 'mark.dickie@gmail.com', rank: 'Acting Pilot Officer', squadron_id: '1', password: 'pepper1899', password_confirmation: 'pepper1899', admin: '1')
